d647764eec0640b5098cb8cbd9f399692c47dd88
Bouziane-Helaili <adressebouziane@hotmail.com>
2022-12-13 16:06:11 +0100

    projet final

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import '../styles/Footer.css'
+
+function Footer() {
+
+	return (
+		<footer className='lmj-footer'>
+			<div className='lmj-footer-elem'>
+				Pour les passionné·e·s de plantes 🌿🌱🌵comme moi. <br />
+				Exercice d'apprentissage de React réalisé à partir d'OpenClassRoom - Bouziane HELAILI
+			</div>
+
+		</footer>
+	)
+}
+
+export default Footer
